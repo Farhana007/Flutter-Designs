74f351e8e9dcf36dcf4126d6af1e13251bd1ed75
Farhana Akter <106755038+Farhana007@users.noreply.github.com>
2024-01-02 12:34:35 -0500

    Update new_design.dart

--- new_design.dart
+++ new_design.dart
@@ -1,4 +1,5 @@
 /// THis File Will contains new Unique Design for  responsive GridStyle Layout 
+///What is MasonryView Is , It gives grid type style but it will be responsive, means it will only take place as much as it need according to it's content
 
 MasonryView(
   
