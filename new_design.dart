16613fbb21ff05c62830e327393a293d97c2e62c
Farhana Akter <106755038+Farhana007@users.noreply.github.com>
2024-01-04 09:21:28 -0500

    responsive grid style using masonry package

--- new_design.dart
+++ new_design.dart
@@ -1,6 +1,18 @@
-/// THis File Will contains new Unique Design for  responsive GridStyle Layout 
-///What is MasonryView Is , It gives grid type style but it will be responsive, means it will only take place as much as it need according to it's content
-
-MasonryView(
-  
-)
+/// This File Will contain new Unique Design for  a responsive GridStyle Layout 
+///What is MasonryView Is, It gives a grid type style but it will be responsive, which means it will only take place as much as it need according to it's content
+ MasonryView(
+          listOfItem: listOfList,
+          itemPadding: 5, //padding between the items you want to have
+          numberOfColumn: 2, //number of columns you can assume this as the number of sliverGridDelegate
+          itemBuilder: (item) { //using masonry view you need to use an item instead of an index
+            ///This is the Column Containing Date and Displaying Items of the List
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 12, top: 10),
+                  child: //anything you want 
+                  ]
+                  );
+          },
+        ),
